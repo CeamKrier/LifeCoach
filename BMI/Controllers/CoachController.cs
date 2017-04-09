7f860a699e972d004c2cc5d781612a10ee6c7112
Emre Çakır <ceamkrier@gmail.com>
2017-04-09 21:50:46 +0300

    Coach Controller and View added
    ../Coach/Calculate has been added

---
+++ BMI/Controllers/CoachController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMI.Controllers
+{
+    public class CoachController : Controller
+    {
+        // GET: Coach
+        public ActionResult Calculate()
+        {
+            return View();
+        }
+    }
+}
